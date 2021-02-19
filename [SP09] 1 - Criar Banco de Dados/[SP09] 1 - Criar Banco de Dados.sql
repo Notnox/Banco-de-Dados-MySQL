c87b93522127ab1643bd029cab189d248778a555
Notnox <74467549+Notnox@users.noreply.github.com>
2021-02-19 11:36:18 -0300

    [SP09] 2 - Criar tabelas

--- [SP09] 1 - Criar Banco de Dados/[SP09] 1 - Criar Banco de Dados.sql
+++ [SP09] 1 - Criar Banco de Dados/[SP09] 1 - Criar Banco de Dados.sql
@@ -1,1 +1,38 @@
-CREATE DATABASE `escola`;
+CREATE DATABASE IF NOT EXISTS `escola`
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS professores (
+	id INT,
+    matricula INT,
+    nome VARCHAR(50),
+    sobrenome VARCHAR(50),
+    endereço VARCHAR(500),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    data_nasicmento DATE,
+    nacionalidade VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS estudantes (
+	id INT,
+    matricula INT,
+    nome VARCHAR(50),
+    sobrenome VARCHAR(50),
+    endereço VARCHAR(500),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    data_nasicmento DATE,
+    nacionalidade VARCHAR(50),
+    media_geral FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS cursos (
+	id INT,
+    codigo INT,
+    nome VARCHAR(100),
+    EAD BOOLEAN,
+    presencial BOOLEAN,
+    professor_responsável VARCHAR(100),
+    número_maximo_alunos_simultâneo INT
+);
